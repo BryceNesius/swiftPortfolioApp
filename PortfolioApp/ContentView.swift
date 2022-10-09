554740cb662a86555fadb3b3dd0a68f6b05591b7
BryceNesius <bnesius@hotmail.com>
2022-10-09 16:16:14 -0400

    initial commit of my swift app

---
+++ PortfolioApp/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  PortfolioApp
+//
+//  Created by Bryce Nesius on 8/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack(alignment: .center) {
+            VStack(alignment: .center) {
+                Text("Midas Design Studio")
+                Spacer()
+                Image("midasdesignlogo120x120")
+                Spacer()
+                
+                
+            }
+        }
+       
+    }
+}
+            
+    
+    
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PortfolioApp/PortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppApp.swift
+//  PortfolioApp
+//
+//  Created by Bryce Nesius on 8/3/22.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
